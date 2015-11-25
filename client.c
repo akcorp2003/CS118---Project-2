4a87ee2128308b59aa925f588985b90b334076c0
akcorp2003 <kuanga2016@ucla.edu>
2015-11-24 22:45:50 -0800

    Incorporated basic transfer functionality
    Added functionality to client with basic data transfer. Also formatted
    server.c.

--- client.c
+++ client.c
@@ -22,20 +22,68 @@ int timeout = 0;
  * 16 bytes total
 */
 
+struct Header
+{
+	short srcPort;
+	short destPort;
+	int seq;
+	int ack;
+	short dataLen;
+	short checkSum;
+};
+
 void error(char *msg)
 {
     perror(msg);
     exit(0);
 }
 
+void getAck(char * buffer, struct Header * packet)
+{
+	memcpy(packet, buffer, HEADER_LEN);
+}
+
+void printPacket(char * packet)
+{
+	struct Header * pktHeader;
+	memcpy(pktHeader, packet, HEADER_LEN);
+
+	printf("srcPort: %d\ndestPort: %d\nseq: %d\n", pktHeader->srcPort,
+		pktHeader->destPort, pktHeader->seq);
+	printf("ack: %d\ndataLen: %d\ncheckSum: %d\n", pktHeader->ack,
+		pktHeader->dataLen, pktHeader->checkSum);
+	printf("%s", packet + HEADER_LEN);
+}
+
+int get_SequenceNumber(char* packet)
+{
+	struct Header* pktHeader;
+	memcpy(pktHeader, packet, HEADER_LEN);
+
+	return pktHeader->seq;
+}
+
+int get_datalen(char* packet)
+{
+	struct Header* pktHeader;
+	memcpy(pktHeader, packet, HEADER_LEN);
+	
+	return (int)(pktHeader->dataLen);
+}
+
 int main(int argc, char *argv[])
 {
+	FILE* file_to_build;
+
     struct hostent* server;
     struct sockaddr_in serv_addr;
     double prob_loss = 0.0;
     double prob_corrupt = 0.0;
     char* filename;
-    int expected_packet_number = 0;
+    int expected_ACK_number = 0;
+	int portno;
+	int sockfd;
+	struct Header header_data;
 
     //check for incorrect input
     if(argc != 6)
@@ -44,14 +92,18 @@ int main(int argc, char *argv[])
         exit(0);
     }
 
-    char buffer[256];
+    char buffer[MAX_PACKETSIZE];
     char packetbuffer[20][MAX_PACKETSIZE];
-    char packet[HEADER_FIELDS][HEADER_LEN];
+    char packet[MAX_PACKETSIZE];
 
     portno = atoi(argv[2]);
     filename = argv[3];
     prob_loss = atof(argv[4]) * 100.0;
     prob_corrupt = atof(argv[5]) * 100.0;
+
+	//for now...
+	prob_loss = 0.0;
+	prob_corrupt = 0.0;
 
     sockfd = socket(AF_INET, SOCK_DGRAM, 0); //create a new socket
     if (sockfd < 0)
@@ -76,45 +128,79 @@ int main(int argc, char *argv[])
 
     int n;
     //send the request over to the server
-    n = write(sockfd,,strlen());
+    n = write(sockfd, filename,strlen(filename));
     if (n < 0)
         error("ERROR writing to socket");
 
     printf("Requested the file: %s\n", filename);
 
+	file_to_build = fopen(filename, "wb");
+
     char* buffer_from_server;
     while(1)
     {
         //run until we receive the FIN packet
-        if(read(sockfd,,strlen() < 0)
+        if(read(sockfd,buffer_from_server,sizeof(buffer_from_server)) < 0)
             error("ERROR reading from socket.");
 
         //packet loss
-        if(rand() % 100 < prob_loss)
+        if(rand() % 100 <= prob_loss)
         {
             //don't do anything special
             printf("A data packet was lost!! The sequence number was: ");
         }
 
         //packet corrpution
-        else if(rand() % 100 < prob_corrupt)
+        else if(rand() % 100 <= prob_corrupt)
         {
             printf("A data packet is corrupted!! The sequence number was: ");
             //send out an ACK that we expected
             //prep up the packet
             sprintf(packet[0], "%d", portno); //src port
-	   //dst port?
-	    sprintf(packet[2], "%d", expected_ack_number - 1); //seq field
-	    if(write(sockfd, packet, strlen(packet)< 0)
-	    {
-		error("Unable to write to sockfd");
-	    }
+		    //dst port?
+			sprintf(packet[2], "%d", expected_ACK_number - 1); //seq field
+			if(write(sockfd, packet, sizeof(packet)) < 0)
+			{
+				error("Unable to write to sockfd");
+			}
 
-        }
+	    }
 
         //nothing bad happened
         else
         {
+			int seq_no = get_SequenceNumber(buffer_from_server);
+			printf("Received packet of seq no.: %d\n", seq_no);
+			fflush(file_to_build);
+			
+
+			//copy all data from the buffer
+			int i;
+			int j = 0;
+			int datalen = get_datalen(buffer_from_server);
+			for (i = HEADER_LEN; i < datalen; i++)
+			{
+				buffer[j] = buffer_from_server[i];
+			}
+
+			fprintf(file_to_build, "%s", buffer);
+			fflush(file_to_build);
+
+			if (seq_no == expected_ACK_number)
+			{
+				//missing srcport for now...
+				header_data.destPort = (short)serv_addr.sin_port;
+				header_data.ack = seq_no;
+				header_data.dataLen = sizeof(short)*2 + sizeof(int);
+				bzero(packet, MAX_PACKETSIZE);
+				memcpy(packet, (void*)&header_data, HEADER_LEN);
+				n = write(sockfd, packet, sizeof(packet));
+				if (n < 0)
+					error("Something went wrong when sending the packet under normal conditions...");
+
+				expected_ACK_number += 1;
+			}
+
 
         }
 
@@ -122,13 +208,11 @@ int main(int argc, char *argv[])
 
     }//end while
 
+	
+	fclose(file_to_build);
+	close(sockfd);
 
-
-
-
-
-
-
+	return 0;
 
 
 }
--- server.c
+++ server.c
@@ -1,613 +1,306 @@
 #include <stdio.h>
-
 #include <sys/types.h>
-
 #include <sys/socket.h>
-
 #include <netinet/in.h>
-
 #include <stdlib.h>
-
 #include <strings.h>
-
 #include <string.h>
 #include <sys/wait.h>
-
 #include <signal.h>
-
 #include <errno.h>
 
-
 static int maxPacketSize = 1000;
-
 static int headerLen = 16;
-
 static int packetBufferSize = 20;
-
 int timeout = 0;
-
 int fileRequest = 1;
 
-
 struct Header {
-
-  short srcPort;
-
-  short destPort;
-
-  int seq;
-
-  int ack;
-
-  short dataLen;
-
-  short checkSum;
-
+	short srcPort;
+	short destPort;
+	int seq;
+	int ack;
+	short dataLen;
+	short checkSum;
 };
 
-
-
 void error(char *msg)
-
 {
-
-  perror(msg);
-
-  exit(1);
-
+	perror(msg);
+	exit(1);
 }
 
-
-
-int isHit (int prob)
-
+int isHit(int prob)
 {
-
-  if (prob == 0)
-
-    return 0;
-
-
-
-  time_t t;
-
-  srand((unsigned) time(&t));
-
-  if (rand() % 100 < prob)
-
-    return 1;
-
-  return 0;
-
+	if (prob == 0)
+		return 0;
+	time_t t;
+	srand((unsigned)time(&t));
+	if (rand() % 100 < prob)
+		return 1;
+	return 0;
 }
 
-
-
-void setTimeout (int signum)
-
+void setTimeout(int signum)
 {
-
-  timeout = 1;
-
+	timeout = 1;
 }
-
-
 
 void getAck(char * buffer, struct Header * packet)
-
 {
-
-  memcpy(packet, buffer, headerLen);
-
+	memcpy(packet, buffer, headerLen);
 }
 
-
-
-
-void printPacket (char * packet)
+void printPacket(char * packet)
 {
-  struct Header * pktHeader;
-  memcpy(pktHeader, packet, headerLen);
-
-  printf("srcPort: %d\ndestPort: %d\nseq: %d\n", pktHeader->srcPort,
-         pktHeader->destPort, pktHeader->seq);
-  printf("ack: %d\ndataLen: %d\ncheckSum: %d\n", pktHeader->ack,
-         pktHeader->dataLen, pktHeader->checkSum);
-  printf("%s", packet + headerLen);
+	struct Header * pktHeader;
+	memcpy(pktHeader, packet, headerLen);
+	printf("srcPort: %d\ndestPort: %d\nseq: %d\n", pktHeader->srcPort,
+		pktHeader->destPort, pktHeader->seq);
+	printf("ack: %d\ndataLen: %d\ncheckSum: %d\n", pktHeader->ack,
+		pktHeader->dataLen, pktHeader->checkSum);
+	printf("%s", packet + headerLen);
 }
 
 int main(int argc, char *argv[])
-
 {
-
-  double probLoss, probCorruption;
-
-  int sockfd, newsockfd, portno, pid, CWnd;
-
-  socklen_t clilen;
-
-  struct sockaddr_in serv_addr, cli_addr;
-
-  struct Header packetHeader;
-
-
-
-  if (argc < 5) {
-
-    fprintf(stderr,"ERROR, missing argument\n");
-
-    exit(1);
-
-  }
-
-
-
-  CWnd = atoi(argv[2]);
-
-  probLoss = atoi(argv[3]);
-
-  probCorruption = atoi(argv[4]);
-
-
-
-  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-
-  if (sockfd < 0) 
-
-    error("ERROR opening socket");
-
-  memset(&serv_addr, 0, sizeof(serv_addr));
-
-  portno = atoi(argv[1]);
-
-  serv_addr.sin_family = AF_INET;
-
-  serv_addr.sin_addr.s_addr = INADDR_ANY;
-
-  serv_addr.sin_port = htons(portno);
-
-     
-
-  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-
-    error("ERROR on binding");
-
-     
-
-  int n, loss = 0, corruption = 0, base = 0, nextSeqnum = 0, seq = 0,
-
-      ack = 0, numUnacked = 0, i, j, k, len, cumAck, sizeCliAddr;
-
-  char buffer[256];
-
-  char packetBuffer[packetBufferSize][maxPacketSize];
-
-  int nxtPtr = 0, nxtFree = 0, count = 0;
-
-  char unacked[CWnd][maxPacketSize+1];
-
-  struct Header recvAck, data;
-
-  char * filename;
-
-  char * temp;
-  FILE * fp;
-
-  char packet[maxPacketSize + 1];
-
-  char cur;
-
-  //col 1: 0 unacked, 1 acked or unused
-
-  //col 2: window seq num
-
-  //col 3: expected ack
-
-  int window[CWnd][3];
-
-
-
-  packet[maxPacketSize] = '\0';
-  for (i = 0; i < CWnd; i++)
-
-  {
-
-    window[i][0] = 1;
-
-    window[i][1] = 0;
-
-    window[i][2] = 0;
-
-  }
-
-  signal (SIGALRM, setTimeout);
-  while (1)
-
-  {
-
-    memset(buffer, 0, 256);
-
-	 	
-
-    // read if timeout has not occured
-
-    if (timeout != 1)
-
-    {
-      sizeCliAddr = sizeof(cli_addr);
-      n = recvfrom(sockfd, buffer, sizeof(buffer), 0, 
-
-                   (struct sockaddr *) &cli_addr, &sizeCliAddr);
-
-   }
-    else n = EINTR;
-
-
-
-    // Timeout action
-
-    if (n == EINTR)
-
-      if (timeout == 1)
-
-      {
-
-        timeout = 0;
-
-
-
-	k = base + CWnd;
-
-	for (i = base; i < k; i++)
-
-          for (j = 0; i < CWnd; j++)
-
-            if (window[j][0] == 0 && window[j][1] == i)
-
-            {
-
-              n = sendto(sockfd, unacked[j], sizeof(unacked[j]), 0,
-
-	                 (struct sockaddr *) &cli_addr, sizeof(cli_addr)); 
-
-              if (n < 0)
-
-                error ("ERROR reading from socket");
-
-
-
-              printPacket (unacked[j]);
-	      break;
-
-            }
-
-
-
-	//signal (SIGALRM, setTimeout);
-
-        alarm (5);
-
-	continue; 
-
-      }
-
-      else
-
-        error("ERROR reading from socket");
-
-    else if (n < 0) error("ERROR reading from socket");
-
-   	 
-
-    // if no loss or corruption
-
-    loss = isHit(probLoss); 
-
-    corruption = isHit(probCorruption);
-
-    if (loss != 1 && corruption != 1)
-
-    {
-
-      // if client asks for file
-
-      if (fileRequest == 1)
-
-      {
-
-        printf("File Requested: %s\n", buffer);
-
-        memcpy(filename, buffer, n);
-
-	filename[n] = '\0';
-
-        fp = fopen(filename, "r");
-
-	
-
-        cur = (char) fgetc(fp);
-
-        len = 0;
-
-        data.srcPort = (short) serv_addr.sin_port;
-
-	data.destPort = (short) cli_addr.sin_port;
-
-	data.ack = 0;
-	for (i = headerLen; cur != EOF; i++)
-
-        {
-
-          if (len < maxPacketSize - headerLen)
-
-          {
-
-            packet[i] = cur; 
-
-	    cur = (char) fgetc(fp);
-
-            len++;
-
-          }
-
-	  else
-
-          {
-
-	    data.seq = seq; 
-
-	    data.dataLen = (short) len;
-
-	    data.checkSum = (short) 1;
-
-	    memcpy(packet, (void *) &data, headerLen);  
-
-	    if (nextSeqnum < base + CWnd)
-
-              for (i = 0; i < CWnd; i++)
-
-                if (window[i][0] == 1)
-
-	        {  
-
-                  window[i][0] = 0;
-
-		  window[i][1] = nextSeqnum;
-
-                  window[i][2] = ack + len;
-
-
-
-                  n = sendto(sockfd, packet, sizeof (packet), 0,
-
-	  	             (struct sockaddr *) &cli_addr, sizeof (cli_addr));
-
-                  if (n < 0)
-
-                    error ("ERROR writing to socket");
-
-
-                  printPacket(packet);
-                  temp = &unacked[i][0];
-                  temp = packet;
-
-
-		  seq += len;	  
-
-                  i = headerLen - 1;
-
-		  len = 0;
-
-		  nextSeqnum++;
-
-
-
-		  break;
-
-                }
-
-            else
-
-            {
-
-              memcpy(packetBuffer[nxtFree], packet,  
-
-                     maxPacketSize);
-
-	      nxtFree++;
-
-	      count++;
-
-            }
-
-          }
-
-        }
-
-	data.seq = seq; 
-
-	data.dataLen = (short) len;
-
-	data.checkSum = (short) 1;
-
-	memcpy(packet, (void *) &data, headerLen);  
-
-	if (nextSeqnum < base + CWnd)
-
-          for (i = 0; i < CWnd; i++)
-
-            if (window[i][0] == 1)
-
-	    {  
-
-              window[i][0] = 0;
-
-	      window[i][1] = nextSeqnum;
-
-              window[i][2] = ack + len;
-
-
-
-              n = sendto(sockfd, packet, sizeof (packet), 0,
-
-	                 (struct sockaddr *) &cli_addr, sizeof (cli_addr));
-
-
-
-              if (n < 0)
-
-                error ("ERROR writing to socket");
-
-
-              printPacket(packet);
-              temp = &unacked[i][0];
-              temp = packet;
-
-	      seq += len;	  
-
-              i = headerLen - 1;
-
-	      len = 0;
-
-	      nextSeqnum++;
-
-
-
-	      break;
-
-            }
-
-        else
-
-        {
-
-          memcpy(packetBuffer[nxtFree], packet,  
-
-                 maxPacketSize);
-
-	  nxtFree++;
-
-	  count++;
-
-        }
-
-
-
-	fclose (fp);
-
-	fileRequest = 0;
-
-      }
-
-      // if client sends ACK
-
-      else
-
-      {
-
-	getAck(buffer, (struct Header *) &recvAck);
-
-	cumAck = recvAck.ack;
-
-	j = 0;
-
-        for (i = 0; i < CWnd; i++)
-
-        {
-
-          if (window[i][1] >= base && cumAck >= window[i][2] &&
-
-              window[i][0] == 0)
-
-          {
-
-            j++;
-
-            window[i][0] = 1;
-
-
-	    if (window[i][1] == base)
-            {
-              //signal (SIGALRM, setTimeout);
-              alarm(0);
-	    }  
-
-	    if (count > 0)
-
-            {
-
-              count--;
-
-	      n = sendto(sockfd, packetBuffer[nxtPtr],
-
-			 sizeof (packetBuffer[nxtPtr]), 0,
-
-	  	         (struct sockaddr *) &cli_addr, sizeof (cli_addr));
-
-
-
-              if (n < 0)
-
-                error ("ERROR writing to socket");
-
-              printPacket(packetBuffer[nxtPtr]);
-
-
-	      nxtPtr = (nxtPtr + 1) % packetBufferSize;
-
-	      nxtFree = (nxtFree + 1) % packetBufferSize;
-
-
-
-              // TODO: add stuff regarding window
-            }
-
-          }
-
-        }
-
-	base += j;
-
-        for (i = 0; i < CWnd; i++)
-          if (window[i][1] == base && window[i][0] == 0)
-          {
-            alarm(5);
-            break;
-          }
-      }
-
-    }
-
-    // if loss or corruption do nothing
-
-    else
-
-    {
-
-      if (loss == 1)
-        printf("Packet Loss\n");
-      if (corruption == 1)
-        printf("Packet Corrupted\n");
-
-      loss = 0;
-
-      corruption = 0;
-
-      
-      printPacket(buffer);
-    }
-
-  }
-
-  close(sockfd);
-
-         
-
-  return 0; 
-
+	double probLoss, probCorruption;
+	int sockfd, newsockfd, portno, pid, CWnd;
+	socklen_t clilen;
+	struct sockaddr_in serv_addr, cli_addr;
+	struct Header packetHeader;
+
+	if (argc < 5) {
+		fprintf(stderr, "ERROR, missing argument\n");
+		exit(1);
+	}
+
+	CWnd = atoi(argv[2]);
+	probLoss = atoi(argv[3]);
+	probCorruption = atoi(argv[4]);
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	if (sockfd < 0)
+		error("ERROR opening socket");
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	portno = atoi(argv[1]);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+
+	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+		error("ERROR on binding");
+
+	int n, loss = 0, corruption = 0, base = 0, nextSeqnum = 0, seq = 0,
+		ack = 0, numUnacked = 0, i, j, k, len, cumAck, sizeCliAddr;
+
+	char buffer[256];
+	char packetBuffer[packetBufferSize][maxPacketSize];
+	int nxtPtr = 0, nxtFree = 0, count = 0;
+	char unacked[CWnd][maxPacketSize + 1];
+	struct Header recvAck, data;
+	char * filename;
+	char * temp;
+	FILE * fp;
+	char packet[maxPacketSize + 1];
+	char cur;
+
+	//col 1: 0 unacked, 1 acked or unused
+	//col 2: window seq num
+	//col 3: expected ack
+	int window[CWnd][3];
+	packet[maxPacketSize] = '\0';
+	for (i = 0; i < CWnd; i++)
+	{
+		window[i][0] = 1;
+		window[i][1] = 0;
+		window[i][2] = 0;
+	}
+	signal(SIGALRM, setTimeout);
+	while (1)
+	{
+		memset(buffer, 0, 256);
+		// read if timeout has not occured
+		if (timeout != 1)
+		{
+			sizeCliAddr = sizeof(cli_addr);
+			n = recvfrom(sockfd, buffer, sizeof(buffer), 0,
+				(struct sockaddr *) &cli_addr, &sizeCliAddr);
+		}
+		else n = EINTR;
+		// Timeout action
+		if (n == EINTR)
+			if (timeout == 1)
+			{
+				timeout = 0;
+				k = base + CWnd;
+				for (i = base; i < k; i++)
+					for (j = 0; i < CWnd; j++)
+						if (window[j][0] == 0 && window[j][1] == i)
+						{
+							n = sendto(sockfd, unacked[j], sizeof(unacked[j]), 0,
+								(struct sockaddr *) &cli_addr, sizeof(cli_addr));
+							if (n < 0)
+								error("ERROR reading from socket");
+							printPacket(unacked[j]);
+							break;
+						}
+				//signal (SIGALRM, setTimeout);
+				alarm(5);
+				continue;
+			}
+			else
+				error("ERROR reading from socket");
+		else if (n < 0) error("ERROR reading from socket");
+		// if no loss or corruption
+		loss = isHit(probLoss);
+		corruption = isHit(probCorruption);
+		if (loss != 1 && corruption != 1)
+		{
+			// if client asks for file
+			if (fileRequest == 1)
+			{
+				printf("File Requested: %s\n", buffer);
+				memcpy(filename, buffer, n);
+				filename[n] = '\0';
+				fp = fopen(filename, "r");
+				cur = (char)fgetc(fp);
+				len = 0;
+				data.srcPort = (short)serv_addr.sin_port;
+				data.destPort = (short)cli_addr.sin_port;
+				data.ack = 0;
+				for (i = headerLen; cur != EOF; i++)
+				{
+					if (len < maxPacketSize - headerLen)
+					{
+						packet[i] = cur;
+						cur = (char)fgetc(fp);
+						len++;
+					}
+					else
+					{
+						data.seq = seq;
+						data.dataLen = (short)len;
+						data.checkSum = (short)1;
+						memcpy(packet, (void *)&data, headerLen);
+						if (nextSeqnum < base + CWnd)
+							for (i = 0; i < CWnd; i++)
+								if (window[i][0] == 1)
+								{
+									window[i][0] = 0;
+									window[i][1] = nextSeqnum;
+									window[i][2] = ack + len;
+									n = sendto(sockfd, packet, sizeof(packet), 0,
+										(struct sockaddr *) &cli_addr, sizeof(cli_addr));
+									if (n < 0)
+										error("ERROR writing to socket");
+									printPacket(packet);
+									temp = &unacked[i][0];
+									temp = packet;
+									seq += len;
+									i = headerLen - 1;
+									len = 0;
+									nextSeqnum++;
+									break;
+								}
+								else
+								{
+									memcpy(packetBuffer[nxtFree], packet,
+										maxPacketSize);
+									nxtFree++;
+									count++;
+								}
+					}
+				}
+				data.seq = seq;
+				data.dataLen = (short)len;
+				data.checkSum = (short)1;
+				memcpy(packet, (void *)&data, headerLen);
+				if (nextSeqnum < base + CWnd)
+					for (i = 0; i < CWnd; i++)
+						if (window[i][0] == 1)
+						{
+							window[i][0] = 0;
+							window[i][1] = nextSeqnum;
+							window[i][2] = ack + len;
+							n = sendto(sockfd, packet, sizeof(packet), 0,
+								(struct sockaddr *) &cli_addr, sizeof(cli_addr));
+							if (n < 0)
+								error("ERROR writing to socket");
+							printPacket(packet);
+							temp = &unacked[i][0];
+							temp = packet;
+							seq += len;
+							i = headerLen - 1;
+							len = 0;
+							nextSeqnum++;
+							break;
+						}
+						else
+						{
+							memcpy(packetBuffer[nxtFree], packet,
+								maxPacketSize);
+							nxtFree++;
+							count++;
+						}
+				fclose(fp);
+				fileRequest = 0;
+			}
+			// if client sends ACK
+			else
+			{
+				getAck(buffer, (struct Header *) &recvAck);
+				cumAck = recvAck.ack;
+				j = 0;
+				for (i = 0; i < CWnd; i++)
+				{
+					if (window[i][1] >= base && cumAck >= window[i][2] &&
+						window[i][0] == 0)
+					{
+						j++;
+						window[i][0] = 1;
+						if (window[i][1] == base)
+						{
+							//signal (SIGALRM, setTimeout);
+							alarm(0);
+						}
+						if (count > 0)
+						{
+							count--;
+							n = sendto(sockfd, packetBuffer[nxtPtr],
+								sizeof(packetBuffer[nxtPtr]), 0,
+								(struct sockaddr *) &cli_addr, sizeof(cli_addr));
+							if (n < 0)
+								error("ERROR writing to socket");
+							printPacket(packetBuffer[nxtPtr]);
+							nxtPtr = (nxtPtr + 1) % packetBufferSize;
+							nxtFree = (nxtFree + 1) % packetBufferSize;
+							// TODO: add stuff regarding window
+						}
+					}
+				}
+				base += j;
+				for (i = 0; i < CWnd; i++)
+					if (window[i][1] == base && window[i][0] == 0)
+					{
+						alarm(5);
+						break;
+					}
+			}
+		}
+		// if loss or corruption do nothing
+		else
+		{
+			if (loss == 1)
+				printf("Packet Loss\n");
+			if (corruption == 1)
+				printf("Packet Corrupted\n");
+			loss = 0;
+			corruption = 0;
+			printPacket(buffer);
+		}
+	}
+	close(sockfd);
+	return 0;
 }
-
-
-
-
-
